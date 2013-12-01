34bd7a0a21bec233472038f2b1da91022940446e
Garreth Dottin <gd7627a@american.edu>
2013-11-30 16:32:37 -0800

    Created locationModule that sends device's current location to an alert on the scene. This location will ultimately be send to the discount API.
    Created sendGeocode Module, to send request to discount API and (to be implemented) deal with response.

--- Resources/alloy/controllers/index.js
+++ Resources/alloy/controllers/index.js
@@ -1,14 +1,7 @@
 function Controller() {
     function doClick() {
-        getLocation();
-    }
-    function getLocation() {
-        if (Ti.Geolocation.locationServicesEnabled) {
-            Titanium.Geolocation.purpose = "Get Current Location";
-            Titanium.Geolocation.getCurrentPosition(function(e) {
-                e.error ? Ti.API.error("Error: " + e.error) : alert(e.coords);
-            });
-        } else alert("Please enable location services");
+        alert(locationModule.fakeLocation);
+        sendGeocode.sendLocation(locationModule.fakeLocation.latitude, locationModule.fakeLocation.longitude);
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "index";
@@ -34,6 +27,37 @@ function Controller() {
     doClick ? $.__views.label.addEventListener("click", doClick) : __defers["$.__views.label!click!doClick"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
+    locationModule = {
+        getLocation: function() {
+            if (Ti.Geolocation.locationServicesEnabled) {
+                Titanium.Geolocation.purpose = "Get Current Location";
+                Titanium.Geolocation.getCurrentPosition(function(e) {
+                    e.error ? Ti.API.error("Error: " + e.error) : alert(e.coords);
+                });
+            } else alert("Please enable location services");
+        },
+        fakeLocation: {
+            accuracy: 30,
+            altitude: 0,
+            altitudeAccuracy: null,
+            heading: 0,
+            latitude: 37.7922852,
+            longitude: -122.4060012,
+            speed: 0,
+            timestamp: 1385426498331
+        }
+    };
+    var sendGeocode = {
+        xhr: Ti.Network.createHTTPClient(),
+        api_url: "www.google.com",
+        sendLocation: function(phoneLatitude, phoneLongitude) {
+            sendGeocode.xhr.open("POST", sendGeocode.api_url);
+            sendGeocode.xhr.send({
+                latitude: phoneLatitude,
+                longitude: phoneLongitude
+            });
+        }
+    };
     $.index.open();
     __defers["$.__views.label!click!doClick"] && $.__views.label.addEventListener("click", doClick);
     _.extend($, exports);
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,32 +1,51 @@
 function doClick(e) {
-		// locationModule.getLocation();
-		locationModule.fakeLocation();
+	// locationModule.getLocation();
+	alert(locationModule.fakeLocation);
+	sendGeocode.sendLocation(locationModule.fakeLocation.latitude, locationModule.fakeLocation.longitude)
 };
 
-var locationModule = {
-	getLocation: function(){
-	  if (Ti.Geolocation.locationServicesEnabled) {
-	    Titanium.Geolocation.purpose = 'Get Current Location';
-	    Titanium.Geolocation.getCurrentPosition(function(e) {
-	        if (e.error) {
-	            Ti.API.error('Error: ' + e.error);
-	        } else {
-	            alert(e.coords);
-	        }
-	    });
-	}
-	else {alert('Please enable location services')}
+locationModule = {
+	getLocation : function() {
+		if (Ti.Geolocation.locationServicesEnabled) {
+			Titanium.Geolocation.purpose = 'Get Current Location';
+			Titanium.Geolocation.getCurrentPosition(function(e) {
+				if (e.error) {
+					Ti.API.error('Error: ' + e.error);
+				}
+				else {
+					alert(e.coords);
+				}
+			})
+		} else {
+			alert('Please enable location services')
+		}
 	},
-	fakeLocation: {
-    "accuracy": 30,
-    "altitude": 0,
-    "altitudeAccuracy": null,
-    "heading": 0,
-    "latitude": 37.7922852,
-    "longitude": -122.4060012,
-    "speed": 0,
-    "timestamp": 1385426498331
-}
-}
-                                          
-$.index.open();
+	fakeLocation : {
+		"accuracy" : 30,
+		"altitude" : 0,
+		"altitudeAccuracy" : null,
+		"heading" : 0,
+		"latitude" : 37.7922852,
+		"longitude" : -122.4060012,
+		"speed" : 0,
+		"timestamp" : 1385426498331
+	}
+};
+
+var sendGeocode = {
+	xhr : Ti.Network.createHTTPClient(),
+	api_url : "www.google.com",
+	sendLocation : function(phoneLatitude, phoneLongitude) {
+		sendGeocode.xhr.open('POST', sendGeocode.api_url);
+		sendGeocode.xhr.send({
+			latitude : phoneLatitude,
+			longitude : phoneLongitude
+		});
+	}
+};
+
+// xhr.onload = function(e) {
+// 
+// };
+
+$.index.open(); 
